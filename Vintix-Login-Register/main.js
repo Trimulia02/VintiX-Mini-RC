5cb03a9377fda2fe7f1e57dd392f3d2b9a12a252
unknown <bimapanjiwijaya@gmail.com>
2026-02-26 22:08:42 +0700

    Login Regis Baru

--- Vintix-Login-Register/main.js
+++ Vintix-Login-Register/main.js
@@ -1,30 +1,22 @@
 var a = document.getElementById("loginBtn");
-var a = document.getElementById("registerBtn");
+var b = document.getElementById("registerBtn");
 var x = document.getElementById("login");
 var y = document.getElementById("register");
 
 function login() {
-  x.style.left = "4px";
-  y.style.right = "-520px";
-  a.className += " white-btn";
-  b.className = "btn";
-  x.style.opacity = 1;
-  y.style.opacity = 0;
-}
-function register() {
-  x.style.left = "-510px";
-  y.style.right = "5px";
-  a.className = "btn";
-  b.className += " white-btn";
-  x.style.opacity = 0;
-  y.style.opacity = 1;
-}
-function register() {
-  document.getElementById("login").style.display = "none";
-  document.getElementById("register").style.display = "block";
+  x.style.display = "block";
+  y.style.display = "none";
+  if (a) {
+    a.className = "btn white-btn";
+    b.className = "btn";
+  }
 }
 
-function login() {
-  document.getElementById("register").style.display = "none";
-  document.getElementById("login").style.display = "block";
+function register() {
+  x.style.display = "none";
+  y.style.display = "block";
+  if (a) {
+    a.className = "btn";
+    b.className = "btn white-btn";
+  }
 }
